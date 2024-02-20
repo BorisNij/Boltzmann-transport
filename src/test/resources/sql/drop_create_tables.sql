5606aad89fdb1ee9e886d3e96897393efdda1d0b
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-02-20 18:55:28 -0500

    feat: change schema defs to use sequences in PKs

--- src/test/resources/sql/drop_create_tables.sql
+++ src/test/resources/sql/drop_create_tables.sql
@@ -1,32 +1,31 @@
-DROP TABLE IF EXISTS groups, courses, students,student_course;
+drop table IF EXISTS groups, courses, students,student_course;
 
-CREATE TABLE  groups (
+create TABLE  groups (
     group_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     group_name VARCHAR(255) NOT NULL,
     CONSTRAINT unique_group_name UNIQUE (group_name)
 );
 
-CREATE TABLE  students (
+create TABLE  students (
     student_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     group_id INT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (group_id) REFERENCES groups (group_id) ON DELETE SET NULL,
-    CONSTRAINT unique_student_group UNIQUE (first_name, last_name)
+    FOREIGN KEY (group_id) REFERENCES groups (group_id) ON delete SET NULL
 );
 
-CREATE TABLE  courses (
-    course_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+create TABLE  courses (
+    course_id SERIAL PRIMARY KEY,
     course_name VARCHAR(255) NOT NULL,
     course_description TEXT,
     CONSTRAINT unique_course_name UNIQUE (course_name)
 );
 
-CREATE TABLE  student_course (
+create TABLE  student_course (
+    id SERIAL PRIMARY KEY,
     student_id int,
     course_id int,
-    primary key (student_id, course_id),
-    foreign key (student_id) references students (student_id) ON DELETE CASCADE,
-    foreign key (course_id) references courses (course_id) ON DELETE CASCADE,
+    foreign key (student_id) references students (student_id) ON delete CASCADE,
+    foreign key (course_id) references courses (course_id) ON delete CASCADE,
     CONSTRAINT unique_student_course UNIQUE (student_id, course_id)
 );
