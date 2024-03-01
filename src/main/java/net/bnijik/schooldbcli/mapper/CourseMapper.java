48b5c615d7089685dc06980fd7b6305bc2f72bdb
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-02-29 22:57:59 -0500

    refactor: extract iterable accepting methods as default methods in the interface

--- src/main/java/net/bnijik/schooldbcli/mapper/CourseMapper.java
+++ src/main/java/net/bnijik/schooldbcli/mapper/CourseMapper.java
@@ -4,11 +4,6 @@ import net.bnijik.schooldbcli.dto.CourseDto;
 import net.bnijik.schooldbcli.entity.Course;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
-
-import java.util.List;
-import java.util.stream.StreamSupport;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public abstract class CourseMapper implements SchoolModelMapper<Course, CourseDto> {
@@ -16,22 +11,6 @@ public abstract class CourseMapper implements SchoolModelMapper<Course, CourseDt
     public abstract CourseDto modelToDto(Course model);
 
     @Override
-    public Slice<CourseDto> modelsToDtos(Iterable<Course> models) {
-        List<CourseDto> courseDtos = StreamSupport.stream(models.spliterator(), false)
-                .map(this::modelToDto)
-                .toList();
-        return new SliceImpl<>(courseDtos);
-    }
-
-    @Override
     public abstract Course dtoToModel(CourseDto dto);
-
-    @Override
-    public Slice<Course> dtosToModels(Iterable<CourseDto> dtos) {
-        final List<Course> coursesModels = StreamSupport.stream(dtos.spliterator(), false)
-                .map(this::dtoToModel)
-                .toList();
-        return new SliceImpl<>(coursesModels);
-    }
 
 }
--- src/main/java/net/bnijik/schooldbcli/mapper/GroupMapper.java
+++ src/main/java/net/bnijik/schooldbcli/mapper/GroupMapper.java
@@ -4,11 +4,6 @@ import net.bnijik.schooldbcli.dto.GroupDto;
 import net.bnijik.schooldbcli.entity.Group;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
-
-import java.util.List;
-import java.util.stream.StreamSupport;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public abstract class GroupMapper implements SchoolModelMapper<Group, GroupDto> {
@@ -16,20 +11,6 @@ public abstract class GroupMapper implements SchoolModelMapper<Group, GroupDto> 
     public abstract GroupDto modelToDto(Group model);
 
     @Override
-    public Slice<GroupDto> modelsToDtos(Iterable<Group> models) {
-        final List<GroupDto> groupDtos = StreamSupport.stream(models.spliterator(), false)
-                .map(this::modelToDto)
-                .toList();
-        return new SliceImpl<>(groupDtos);
-    }
-
-    @Override
     public abstract Group dtoToModel(GroupDto dto);
-
-    @Override
-    public Slice<Group> dtosToModels(Iterable<GroupDto> dtos) {
-        final List<Group> groupModels = StreamSupport.stream(dtos.spliterator(), false).map(this::dtoToModel).toList();
-        return new SliceImpl<>(groupModels);
-    }
 
 }
